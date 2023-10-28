a933d0146fad5cc918e9beac31c8d1fe4249c803
student <student@gnu.ac.in>
2023-10-28 09:43:03 +0530

    pr10

--- app/src/main/java/com/example/mad_practical10_21012011002/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical10_21012011002/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
                 try {
                     val data = HttpRequest().makeServiceCall(
                         "https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
-                        "dchj8v1b6qqdjzbqood1jgpachyfzlw58r540gru")
+                        "rbn0rerl1k0d3mcwgw7dva2xuwk780z1hxvyvrb1")
                     withContext(Dispatchers.Main) {
                         try {
                             if(data != null)
@@ -58,18 +58,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun setarraytolistview(){
-        val personlistview = findViewById<ListView>(R.id.listview)
-        val array = arrayListOf<Person>(
-            Person("0484","Chintan","abc@gmail.com","7687547689","mehsana",90.0,90.7),
-            Person("11","Sagar","def@gmail.com","6689563456","Rajkot",90.90,909.90),
-            Person("15","Dev","hij@gmail.com","5621459455","Junagadh",75.63,45.75),
-            Person("29","Bhargav","jkl@gmail.com","1254786532","Veraval",45.21,36.90),
-            Person("31","Vivek","xyz@gmail.com","5485659875","Junagadh",45.90,78.21)
-        )
-        val swap=findViewById<FloatingActionButton>(R.id.btnSwap)
-        swap.setOnClickListener{
 
-        }
-    }
+
 }
